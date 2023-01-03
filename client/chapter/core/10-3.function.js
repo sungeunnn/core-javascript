e1a262c90e7cc4c5d9b5cff38d3d6f0f7b084172
sungeunnn <fronteun@naver.com>
2023-01-03 15:53:07 +0900

    화살표 함수 update

--- client/chapter/core/10-3.function.js
+++ client/chapter/core/10-3.function.js
@@ -1,54 +1,90 @@
 /* ---------------------------------------------------------------------- */
 /* Functions → Arrow                                                      */
 /* ---------------------------------------------------------------------- */
-
+/* 
 let arr = [1,2,3,4];
-
-//rest parameter
 function sum(...args){
   console.log(args);
 }
+ */
 
-sum(1,2,3,4,)
-
-
-
-const calculateTotal = (... args) =>{
+// sum(1,2,3,4,)
+// rest parameter
+const calculateTotal = (...args) => {
   let total = 0;
-  
-  args.forEach(item => {
-    total += item
-  })
 
-  args.reduce((acc,item) => acc+ item)
+  args.forEach((item) => {
+    total += item;
+  });
 
-  console.log();
+  // args.reduce((acc,item)=> acc + item )
+  // console.log();
 
-  return ;
-}
+  return total;
+};
 
 let resultX = calculateTotal(10000, 8900, 1360, 2100);
 // let resultY = calculateTotal(21500, 3200, 9800, 4700);
 // let resultZ = calculateTotal(9000, -2500, 5000, 11900);
 
-/* console.log(resultX);
-console.log(resultY);
-console.log(resultZ); */
-
+console.log(resultX);
+// console.log(resultY);
+// console.log(resultZ);
 
 // 함수 선언 → 화살표 함수 (표현)식
-let calcAllMoney = (a,b,c,d) => {
-  return a+b+c+d;
-};
-
+let calcAllMoney = (a, b, c, d) => a + b + c + d;
 
 // 화살표 함수와 this
+//일반함수는 나를 호출한 대상을 전역으로 삼는다
+// 화살표 함수는 호출한 대상이 아닌 전역인 window를 가르킨다.
 
+function normalFunction() {
+  console.log("일반함수 : ", this);
+}
+
+const arrowFunction = () => {
+  console.log("화살표함수 : ", this);
+};
+
+// normalFunction()
+// arrowFunction()
+
+//  객체의 메서드로 함수를 실행할 경우는 일반함수로 실행해야 this가 객체를 찾습니다.
+//  메서드 안에서의 함수는 this를 찾기 위해서는 arrowFunction이 오히려 좋아.
+
+const user = {
+  name: "tiger",
+  age: 32,
+  address: "서울시 중랑구 면목동",
+  grades: [80, 90, 100],
+  totalGrades: function () {
+    /*  let total = 0;
+    this.grades.forEach((item)=>{
+      this
+    })
+    return total */
+
+    function foo() {
+      console.log("foo : ", this);
+    }
+
+    const bar = () => {
+      console.log("bar : ", this);
+    };
+
+    foo.call(user); // user
+    bar(); // user object
+  },
+};
+
+console.log(user.totalGrades());
 
 /* 다음 함수를 작성해봅니다. -------------------------------------------------- */
 
 // pow(numeric: number, powerCount: number): number;
-let pow; 
+let pow;
+//반복문으로 거듭제곱 구현
 
 // repeat(text: string, repeatCount: number): string;
-let repeat; 
+let repeat;
+//string을 repeatCount만큼 반복
