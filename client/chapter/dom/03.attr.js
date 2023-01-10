e1da1c1c651c8040cda0b10a8bf640f316c23b56
sungeunnn <fronteun@naver.com>
2023-01-10 13:32:44 +0900

    attr set함수 학습 및 수정

--- client/chapter/dom/03.attr.js
+++ client/chapter/dom/03.attr.js
@@ -72,19 +72,25 @@ function getAttr(node,prop){
 getAttr('.first', 'data-play');
 
 
+// - elementNode.dataset
 //set함수 만들기
-
+// set은 속성을 추가하고, 값을 넣기
 function setAttr(node, prop, value){
-  if(typeof node == 'string'){
-    node = getNode(node);
+  if(typeof node == 'string') node = getNode(node);
+  if(typeof prop !== 'string') throw new TypeError('setAttr 함수의 두번째 인자는 문자 타입 입니다.');
+  
+  if(prop.includes('data')){
+    let rest = prop.slice(5);
+    node.dataset(rest) = value;
   }
-  return node.setAttribute(prop, value);
+  if(!value) throw new SyntaxError('setAttr 함수의 세번 째 인자는 필수 값 입니다.')
+
+  //함수를 통해 무엇을 얻고자 하는게 아니기 때문에 return을 쓰지 않아도 된다.
+  node.setAttribute(prop, value);
 }
 
 setAttr('.first', 'data-value', 'hello');
 
 
+//get,set을 모두 사용하는 함수
 
-
-
-// - elementNode.dataset
