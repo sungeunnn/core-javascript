4ee52492ddd6cac8cccbd1a7f7b1824f88b12b70
sungeunnn <fronteun@naver.com>
2023-01-10 10:38:07 +0900

    nodeClass 학습

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,17 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "node": true
-    },
-    "extends": "eslint:recommended",
-    "overrides": [
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "rules": {
-        "no-unused-vars": "off"
-    }
-}
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: "eslint:recommended",
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    "no-unused-vars": "off",
+    "no-undef": "warn", /*import export  배우기 전이라서 오류 안뜨게 해줄려고 한 것 좋은방법 아님 (선언안한게 표시가 안됌)*/ 
+  },
+};
