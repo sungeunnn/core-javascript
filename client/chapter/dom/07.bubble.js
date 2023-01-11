3805481ba11357551d951c5fb500d93785036079
sungeunnn <fronteun@naver.com>
2023-01-11 16:02:41 +0900

    버블링 학습

--- client/chapter/dom/07.bubble.js
+++ client/chapter/dom/07.bubble.js
@@ -1,15 +1,43 @@
-
 /* ---------------------------------------------------------------------- */
-/* Event Handling                                                         */
+/* Event bubbling & capturing                                             */
 /* ---------------------------------------------------------------------- */
 
-/* 이벤트 핸들링 3가지 방법 --------------------------------------------------- */
+/* 버블링 ----------------------------------------------------------------- */
 
-// 1. HTML 속성 : onclick="handler()"
-// 2. DOM 프로퍼티 : element.onclick = handler
-// 3. 메서드 : element.addEventListener(event, handler[, phase])
+const visual = getNode('.visual');
+const news = getNode('.news');
+const desc = getNode('.desc');
 
-/* 이벤트 추가/제거 --------------------------------------------------------- */
+/* visual.addEventListener('click', function(e){
+  console.log('target : ',e.target);
+  console.log('currentTarget: ',e.currentTarget);
+  console.log('%c visual','background:dodgerblue');
+}) */
 
-// - addEventListener
-// - removeEventListener
+visual.addEventListener('click',(e)=>{
+  let elem = e.currentTarget;
+  // console.log('target : ',e.target);
+  // console.log('currentTarget: ',e.currentTarget);
+  console.log('%c visual','background:dodgerblue');
+  css('.pop','display','block');
+  
+})
+
+getNode('.pop').addEventListener('click',(e)=>{
+  //버블링 중단이  없을 때 왜 작동되지 않는것처럼 보이냐면 눌러짐과 동시에 사라졌다 다시 생기기 때문이다
+  // e.stopPropagation();   
+  css('.pop','display','none');
+})
+
+
+/* news.addEventListener('click', function(e){
+  e.stopPropagation();
+  console.log('%c news','background:orange');
+})
+
+desc.addEventListener('click', function(e){
+  e.stopPropagation();
+  console.log('%c desc','background:pink');
+}) */
+
+/* 캡처링 ----------------------------------------------------------------- */
