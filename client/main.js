8a8e4e3a07a4055fea54d0aa7bb315d6fcb05a5c
sungeunnn <fronteun@naver.com>
2023-01-19 11:04:38 +0900

    배열로 받아온 response 각 렌더링 학습 및 gsap 세팅

--- client/main.js
+++ client/main.js
@@ -1,15 +1,22 @@
-import { insertLast, ggom, renderUserCard, getNode } from "./lib/index.js";
+/* global gsap */
+import { ggom, renderUserCard, getNode } from "./lib/index.js";
 
 
 const userCardContainer = getNode('.user-card-inner');
 //rendingUserList
 async function rendingUserList(){
-  let response = await ggom.get('https://jsonplaceholder.typicode.com/users/1')
+  let response = await ggom.get('https://jsonplaceholder.typicode.com/users')
   let userData=response.data;
   
+/*   for(let i =0; i<userData.length; i++){
+    renderUserCard(userCardContainer, userData[i])
+  } */
   
+  userData.forEach((data)=>{
+    renderUserCard(userCardContainer, data)
+  })
 
- renderUserCard(userCardContainer, userData)
+
 }
 rendingUserList()
 
