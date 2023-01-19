a23cb0d303215276cf3e705063aeeb19e459f482
sungeunnn <fronteun@naver.com>
2023-01-19 15:54:40 +0900

    localstorage [생성, 가져오기, 삭제] 사용 학습

--- client/main.js
+++ client/main.js
@@ -1,1 +1,20 @@
-import {  } from "./lib/index.js";
+import { delStorage, getNode, loadStorage, saveStorage } from "./lib/index.js";
+
+const textField = getNode("#textField");
+const deleteButton = getNode('input[value="삭제"]');
+
+loadStorage('area').then((res)=>{
+  textField.value = res;
+})
+
+
+function inputHandler() {
+  saveStorage("area", textField.value);
+}
+
+function delHandler(){
+  delStorage("area")
+}
+
+textField.addEventListener("input", inputHandler);
+deleteButton.addEventListener('click', delHandler);
