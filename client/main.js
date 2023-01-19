25acfdd1d62d2a73aac5c0040cb1754564beba5d
sungeunnn <fronteun@naver.com>
2023-01-19 15:04:08 +0900

    ToyPJ 사용자 명함카드 삭제 학습

--- client/main.js
+++ client/main.js
@@ -1,27 +1,68 @@
 /* global gsap */
-import { ggom, renderUserCard, getNode } from "./lib/index.js";
+import { 
+  ggom, 
+  renderUserCard, 
+  getNode as $, 
+  changeColor, 
+  delayP,
+  renderSpinner ,
+  renderEmptyCard,
+  attr,
+} from "./lib/index.js";
 
 
-const userCardContainer = getNode('.user-card-inner');
+const userCardContainer = $('.user-card-inner');
+
 //rendingUserList
 async function rendingUserList(){
-  let response = await ggom.get('https://jsonplaceholder.typicode.com/users')
-  let userData=response.data;
+  renderSpinner(userCardContainer);
+
+  try{
+    await delayP(1000)
+    $('.loadingSpinner').remove();
+    let response = await ggom.get('http://localhost:3000/users')
+    let userData=response.data;
+    
+  /*   for(let i =0; i<userData.length; i++){
+      renderUserCard(userCardContainer, userData[i])
+    } */
+    
+    userData.forEach((data)=>{
+      renderUserCard(userCardContainer, data)
+    })
   
-/*   for(let i =0; i<userData.length; i++){
-    renderUserCard(userCardContainer, userData[i])
-  } */
+    changeColor('.user-card')
   
-  userData.forEach((data)=>{
-    renderUserCard(userCardContainer, data)
-  })
+    gsap.to(gsap.utils.toArray('.user-card'),{
+      x:0, 
+      opacity:1, 
+      duration:1.5,
+      stagger:0.2 ,
+    })
+  }catch(err){
+    renderEmptyCard(userCardContainer);
+  }
 
 
 }
 rendingUserList()
 
 
+function delHandler(e){
+  let deleteBtn = e.target.closest('button');
+  let article = e.target.closest('article');
 
+  if(!deleteBtn || !article) return
+  
+  let id = attr(article, 'data-index').slice(5);
+  
+  ggom.delete(`http://localhost:3000/users/${id}`).then(()=>{
+    userCardContainer.innerHTML = '';
+    rendingUserList();
+  })
+}
+
+userCardContainer.addEventListener('click',delHandler)
 
 
 
